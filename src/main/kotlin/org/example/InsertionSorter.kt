5b7f16246eb5287e9b4a33fe5199ed9c6ae62606
Alex Karasev <alex.v.karasev@gmail.com>
2018-07-23 07:31:22 +0700

    Implemented insertion sort.

---
+++ src/main/kotlin/org/example/InsertionSorter.kt
@@ -0,0 +1,16 @@
+package org.example
+
+class InsertionSorter {
+
+    fun sort(a: IntArray): Unit {
+        for (j in 1 until a.size) {
+            val key = a[j]
+            var i = j - 1
+            while (i >= 0 && a[i] > key) {
+                a[i + 1] = a[i]
+                i--
+            }
+            a[i + 1] = key
+        }
+    }
+}
